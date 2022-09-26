e194cef7434bf3ecfbca0203c7983d7661956e0a
SHARATHbhushan <sharathbhushan18@gmail.com>
2022-09-26 18:34:51 +0200

    fix ed_tts

--- src/tts/src/ed_tts.py
+++ src/tts/src/ed_tts.py
@@ -1,18 +1,18 @@
 import asyncio
 import tempfile
-
+import soundfile
 from playsound import playsound
-
+import wave
 import edge_tts 
-
-
-
-import espeakng
-import time
+import pyaudio
+import librosa
+import soundfile as sf  
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Bool
 
+p = pyaudio.PyAudio()
+CHUNK = 1024
 class tts_engine():
     import asyncio
     def __init__(self):
@@ -31,11 +31,36 @@ class tts_engine():
 
         communicate = edge_tts.Communicate()
         ask = phrase
-        with tempfile.NamedTemporaryFile() as temporary_file:
+        with open('response.wav', 'wb') as temporary_file:
             async for i in communicate.run(ask, voice="Microsoft Server Speech Text to Speech Voice (en-US, GuyNeural)"):
                 if i[2] is not None:
                     temporary_file.write(i[2])
-            playsound(temporary_file.name)
+            #sd.play(temporary_file.name, 44100)
+            print(temporary_file.name)
+            x,_ = librosa.load(temporary_file.name, sr=16000)
+            sf.write('tmp.wav', x, 16000)
+            wave.open('tmp.wav','r')
+
+            # Now try to open the file with wave
+            wf = wave.open("tmp.wav", 'rb')
+            stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+            data = wf.readframes(CHUNK)
+            while len(data):
+                stream.write(data)
+                data = wf.readframes(CHUNK)
+            '''
+            with open("response.raw", "rb") as inp_f:
+                data = inp_f.read()
+                with wave.open("sound.wav", "wb") as out_f:
+                    out_f.setnchannels(1)
+                    out_f.setsampwidth(2) # number of bytes
+                    out_f.setframerate(44100)
+                    out_f.writeframesraw(data)
+            '''
+            #playsound(temporary_file.name)
         print("worked")
 
     
